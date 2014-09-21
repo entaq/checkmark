dbad120af99e1e71aa1511be6d470b15b2d66c07
Arun Nagarajan <arun@isapp.com>
2014-09-21 09:44:07 -0400

    improved routes and added sample search

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Clearmark::Application.routes.draw do
+  root 'search#index'
   get "search/index"
   get "search/alerts"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
